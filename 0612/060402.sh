39943a74d2d37a7ac7ddd530b740f5d603ee6fb2
s19013 <s19013@std.it-college.ac.jp>
2019-06-25 19:17:37 +0900

    1

--- 0612/060402.sh
+++ 0612/060402.sh
@@ -1,3 +1,11 @@
+#usage()
+#{
+#cat << END
+#FILEPATHで指定したファイルの容量を表示
+#FILEPATH-容量を求めるファイルのパスを指定、複数指定可
+#END
+#}
+
 if [ "$#" -eq 0 ]; then
   echo "使い方：060402.sh FILEPATH ..."
   echo "FILEPATHで指定したファイルの容量を表示"

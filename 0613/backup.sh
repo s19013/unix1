81bcc44a97076cbf67b860a3eee4cc4f69abab6d
s19013 <s19013@std.it-college.ac.jp>
2019-06-25 19:21:19 +0900

    1

--- 0613/backup.sh
+++ 0613/backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd /tmp
-tar czf .tar home- `date '+%Y%m%d'`.tar.gz -x ~/ex.conf "$HOME"
+#cd /tmp
+#tar czf .tar home- `date '+%Y%m%d'`.tar.gz -x ~/ex.conf "$HOME"
 #ex.conf を読み込む
+
+day=$(date +'%Y%m%d')
+#tar cf - ~ -X ~/ex.cnf | gzip -c > /tmp/home-${day}.tar.gz
+tar czf /tmp/home-${day}.tar.gz -X ~/ex.cnf ~/

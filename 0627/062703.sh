0ae4ee196366c5ee9af83c8234f80d0a331431d4
s19013 <s19013@std.it-college.ac.jp>
2019-06-27 10:40:39 +0900

    test

--- 0627/062703.sh
+++ 0627/062703.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 in=$1
+
+if [ "$#" -eq 0 ]; then
+  echo "使い方：062703.sh  DIRPATH ..."
+  echo "DIRPATHで指定したディレクトリ下の実行可能ファイル数/ディレクトリ数を表示"
+  echo "DIRPATH - ファイル数/ディレクトリ数を表示するディレクトリパスを指定"
+fi
+
+
 if [ -d "$in" ]; then
     file=`find $in -type f | wc -l`
     echo "file =>${file}"
